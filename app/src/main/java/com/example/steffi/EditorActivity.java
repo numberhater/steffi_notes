2159268323b99fd90093fc9920af8b2d1b6ea235
Numberhater <ben.min.newman@gmail.com>
2019-12-26 21:39:24 -0800

    working version from demo

---
+++ app/src/main/java/com/example/steffi/EditorActivity.java
@@ -0,0 +1,141 @@
+package com.example.steffi;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class EditorActivity extends AppCompatActivity {
+
+    private String action;
+    private EditText editor;
+    private String noteFilter;
+    private String oldText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editor = findViewById(R.id.editText2);
+
+       Intent intent = getIntent();
+       Uri uri = intent.getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
+
+       if(uri ==null){
+           action = Intent.ACTION_INSERT;
+           setTitle(R.string.new_note);
+       } else {
+           action = Intent.ACTION_EDIT;
+           noteFilter = DBOpenHelper.NOTE_ID + "=" + uri.getLastPathSegment();
+
+           Cursor cursor = getContentResolver().query(uri,
+                   DBOpenHelper.ALL_COLUMNS,
+                   noteFilter,
+                   null,
+                   null);
+
+           cursor.moveToFirst();
+           oldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+
+           editor.setText(oldText);
+           editor.requestFocus();
+
+       }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (action.equals(Intent.ACTION_EDIT)){
+            getMenuInflater().inflate(R.menu.menu_editor, menu);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finishEditing();
+                break;
+
+            case R.id.action_delete:
+                deleteNotes();
+                break;
+        }
+
+        return true;
+    }
+
+    private void deleteNotes() {
+        getContentResolver().delete(NotesProvider.CONTENT_URI, noteFilter, null);
+        Toast.makeText(this,R.string.note_deleted, Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    private void finishEditing(){
+        String newText = editor.getText().toString().trim();
+
+        switch (action){
+            case Intent.ACTION_INSERT:
+                if (newText.length() == 0){
+                    setResult(RESULT_CANCELED);
+                } else {
+                    insertNote(newText);
+                }
+                break;
+            case Intent.ACTION_EDIT:
+                if (newText.length() == 0){
+                    deleteNotes();
+
+                } else if (oldText.equals(newText)){
+                    setResult(RESULT_CANCELED);
+                } else {
+                    updateNote(newText);
+                }
+            }
+
+        finish();
+    }
+
+    private void updateNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().update(NotesProvider.CONTENT_URI, values, noteFilter, null);
+        Toast.makeText(this, R.string.note_updated, Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        setResult(RESULT_OK);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishEditing();
+    }
+}
